41f22093f1ef7c76124ee302184106cb71a75da9
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-07-06 00:15:09 +0200

    setup svelte kit, scss and supabase start

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from '$lib/Header/index.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer />
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>clippath</h1>
+
+	<p>lorem ipsum</p>
+</section>
+
+<style lang="scss">
+	section {
+		h1 {
+			color: darkorchid;
+		}
+
+		p {
+			color: hotpink;
+		}
+	}
+</style>
